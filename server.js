82e96facf24a2e562403f0749af4586a896ff1e7
Dan Kincaid <dankincaid@Dans-MacBook-Pro.local>
2018-01-30 21:44:29 -0800

    Set Up API router and Four modules

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorhandler = require('errorhandler');
 const express = require('express');
-
-// const apiRouter = require('./api/api');
+const apiRouter = require('./api/api');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -11,6 +10,7 @@ const PORT = process.env.PORT || 4000;
 app.use(bodyParser.json());
 app.use(cors());
 app.use(errorhandler());
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
   console.log('Listening on port: ' + PORT);
